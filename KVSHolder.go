675d4f9c6e28fe8e422b4070400d4e1601e3f837
Vlad Oblamskyi <vlad.oblamskyi@intellecteu.com>
2017-01-27 11:32:09 +0200

    Fix

--- KVSHolder.go
+++ KVSHolder.go
@@ -4,17 +4,12 @@ import (
 	"errors"
 	"fmt"
 	"github.com/hyperledger/fabric/core/chaincode/shim"
-	"github.com/op/go-logging"
-	"strings"
 )
-
-var myLogger = logging.MustGetLogger("kvs_holder")
 
 type KVSHolder struct {
 }
 
 func (t *KVSHolder) Init(stub shim.ChaincodeStubInterface, function string, args []string) ([]byte, error) {
-	myLogger.Info("Inited...")
 	return nil, nil
 }
 
@@ -22,12 +17,10 @@ func (t *KVSHolder) Invoke(stub shim.ChaincodeStubInterface, function string, ar
 	switch function {
 	case "put":
 		for i := 0; i < len(args); i += 2 {
-			myLogger.Debugf("Put tuple: [{%s}, {%s}]", args[i], args[i + 1])
 			stub.PutState(args[i], []byte(args[i + 1]))
 		}
 		return nil, nil
 	default:
-		myLogger.Errorf("Unsupported operation %s", function)
 		return nil, errors.New("Unsupported operation")
 	}
 }
@@ -35,7 +28,6 @@ func (t *KVSHolder) Invoke(stub shim.ChaincodeStubInterface, function string, ar
 func (t *KVSHolder) Query(stub shim.ChaincodeStubInterface, function string, args []string) ([]byte, error) {
 	state, _ := stub.GetState(args[0])
 	result := string(state)
-	myLogger.Infof("Query result [{%s}] for {%s}", result, strings.Join(args, ","));
 	return []byte(result), nil
 }
 
